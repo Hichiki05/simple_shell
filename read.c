656b492e45055cb8375b6ea3253b37c715da7864
Hichiki05 <safaeelbazofficial@gmail.com>
2023-11-09 16:45:05 +0100

    2

--- read.c
+++ read.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/*
-*read - a fonction the read the user input
-@command: a string
-@size: the size of the command
+/**
+* _read - a fonction the read the user input
+* Return: the sent
 */
 
-char read(void)
+char *_read(void)
 {
 	char *sent = NULL;
 	size_t lent = 0;
@@ -14,10 +13,12 @@ char read(void)
 
 	if (isatty(STDOUT_FILENO))
 	write(STDOUT_FILENO, "$ ", 2);
+
 	i = getline(&sent, &lent, stdin);
 	if (i == -1)
 	{
-		free(sent);
+		free(sent), sent = NULL;
+		
 		return (NULL);
 	}
 	return (sent);
