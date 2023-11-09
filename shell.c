423aad0bb5e6d1c734076c8bfc2f04b0970d0460
Hichiki05 <safaeelbazofficial@gmail.com>
2023-11-09 16:45:35 +0100

    4

--- shell.c
+++ shell.c
@@ -1,29 +1,34 @@
 #include "main.h"
 /**
-*main - the main fonction for the simple shell project
+* main - the main fonction for the simple shell project
+* @ac: argc
+* @argv: the arguments and the name of the program
+* Return: NULL
 */
+
 
 int main(int ac, char **argv)
 {
 	char *sent = NULL;
 	char **command = NULL;
-	char status = 0;
+	char status = 0, indx = 0;
 	(void) ac;
 
 	while (1)
 	{
-		sent = read();
+		sent = _read();
 		if (sent == NULL)
 		{
 			if (isatty(STDOUT_FILENO))
 			write(STDOUT_FILENO, "\n", 1);
 			return (status);
 		}
+		indx++;
 
 		command = token(sent);
 		if (!command)
 		continue;
 
-	status = execute(command, argv);
+	status = execute(command, argv, indx);
 }
 }
