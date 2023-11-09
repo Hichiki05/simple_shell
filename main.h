d2cf2bd9e01af8f726b333b6ba7661e49924683c
Hichiki05 <safaeelbazofficial@gmail.com>
2023-11-09 16:44:50 +0100

    1

--- main.h
+++ main.h
@@ -1,26 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-*the header file
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+/*
+*SEP: a delimitor
+*/
 
 #define SEP " \t\n"
 extern char **environ;
 
-char read(void);
-int execute(char **command, char **argv);
-void free(char **mess);
+char *_read(void);
+int execute(char **command, char **argv, int indx);
 char **token(char *sent);
+char *_env(char *var);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+char *path(char *sent);
+void _error(char *name, char *comm, int indx);
+char *_itoa(int i);
+void reverse_str(char *string, int lent);
 
 #endif
