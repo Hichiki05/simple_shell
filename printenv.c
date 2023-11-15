239d4f8c1a105e6cf9aa00e4622188ad78e3a0d7
sillah0 <siladennis624@gmail.com>
2023-11-15 12:56:22 +0300

    new update

--- printenv.c
+++ printenv.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
+/**
+ * print_env - function to print environment
+ * @info: parameter
+*/
 
 void print_env(info_t *info)
 {
-    int i = 0;
+	int i = 0;
 
-    while(environ[i])
-    {
-        write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-        write(STDOUT_FILENO, "\n", 1);
-        i++;
-    }
-    info->args[0] = NULL;
+	while (environ[i])
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+	info->args[0] = NULL;
 }
